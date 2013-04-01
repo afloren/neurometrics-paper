d9c5435a5e08790a5c4d97ed10897b70534bc174
Andrew Floren <andrewfloren@gmail.com>
2013-04-01 18:50:58 -0500

    added initial wiener deconvolution figure

---
+++ matlab/GenerateWienerDeconvolutionFigure.m
@@ -0,0 +1,37 @@
+dt = 0.5;
+
+h = MakeHRF(dt,256);
+
+x = square(0.2*(1:144));
+
+y = conv(x,h,'full');
+
+xhat = WienerDeconv(y,MakeHRF(dt,length(y)),1);
+
+figure(1);
+clf;
+
+subplot(1,3,1);
+plot(dt*(1:length(h)),h);
+
+subplot(1,3,2);
+hold on;
+plot(dt*(1:length(x)),x,'r');
+plot(dt*(1:length(y)),y,'g');
+plot(dt*(1:length(xhat)),xhat,'b');
+
+load example_voxel;
+
+dt = 2.5;
+
+y = example_voxel;
+
+xhat = WienerDeconv(y,MakeHRF(dt,length(y)),1);
+
+subplot(1,3,3);
+hold on;
+plot(dt*(1:length(y)),y,'r');
+plot(dt*(1:length(xhat)),xhat,'b');
+
+
+
